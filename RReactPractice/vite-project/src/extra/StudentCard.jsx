f9d6100865d078562c96ac49bf24b8f027a95a3f
unknown <samirbaniya500@gmail.com>
2024-08-28 20:52:24 +0545

    Photo added student card

--- RReactPractice/vite-project/src/extra/StudentCard.jsx
+++ RReactPractice/vite-project/src/extra/StudentCard.jsx
@@ -6,87 +6,99 @@ function StudentCard() {
       roll: 1,
       address:"Banepa",
       subject:"C++",
-      image: <img className="imga" src="1.png" alt="1" />
+      image: "1.png"
     },
     {
       name: "Raj",
       roll: 2,
       address:"Panauti",
       subject:"Java",
-      image: <img className="imga" src="2.png" alt="2" />
+      image: "2.png"
 
     },
     {
       name: "Rajan",
       roll: 4,
       address:"Banepa",
-      subject:"Python"
+      subject:"Python",
+      image: "beard.png"
     },
     {
       name: "Gopal",
       roll: 5,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "fb.png"
     },
     {
       name: "Harka",
       roll: 6,
       address:"Bhaktapur",
-      subject:"C++"
+      subject:"C++",
+      image: "logo.png"
     },
     {
       name: "Hari",
       roll: 55,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "icon.png"
     },
     {
       name: "Sravan",
       roll: 11,
       address:"Nala",
-      subject:"C++"
+      subject:"C++",
+      image: "shop.png"
     },
     {
       name: "Rajesh",
       roll: 7,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "yt.png"
     },
     {
       name: "Sheldon",
       roll: 12,
       address:"Banepa",
-      subject:"OOP"
+      subject:"OOP",
+      image: "fb.png"
     },
     {
       name: "Alex",
       roll: 8,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "logo.png"
     },
     {
       name: "Luke",
       roll: 9,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "logo.png"
     },
     {
       name: "Charlie",
       roll: 10,
       address:"Banepa",
-      subject:"Math"
+      subject:"Math",
+      image: "logo.png"
     },
     {
       name: "Alan",
       roll: 15,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "logo.png"
     },
     {
       name: "Alan",
       roll: 22,
       address:"Banepa",
-      subject:"C++"
+      subject:"C++",
+      image: "logo.png"
     },
     
   ];
@@ -98,7 +110,7 @@ function StudentCard() {
         <div>Roll:{item.roll}</div>
         <div>Address:{item.address}</div>
         <div>Subject:{item.subject}</div>
-        <div>Image: <br/> {item.image}</div>
+        <div>Image: <br/> <img className="imga" src={item.image} alt={item.image} /></div>
 
       </div>
     </div>
